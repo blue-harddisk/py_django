62a766bb4214168c85ab4f0317e7f7b76f2540a6
windows <windows@163.com>
2018-07-17 20:08:13 +0800

    使用模板,创建静态文件夹

--- py_django/settings.py
+++ py_django/settings.py
@@ -120,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# 告知Django静态文件保存在哪个目录下
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_files')]
--- users/views.py
+++ users/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     """访问首页的视图"""
-    return HttpResponse("Hello Django")
+    return render(request, "users/ThinkPad.html")
