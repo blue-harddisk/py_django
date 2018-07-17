f21dc6a544581aa22d0197e4e40a4fd1942301fc
windows <windows@163.com>
2018-07-17 11:04:22 +0800

    创建项目,print(hello,django)

---
+++ users/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    """访问首页的视图"""
+    return HttpResponse("hello django")
