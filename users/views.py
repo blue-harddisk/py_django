20883d962b6aba2eed588458f561bbbde25d9594
windows <windows@163.com>
2018-07-17 11:39:11 +0800

    look look

--- users/views.py
+++ users/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     """访问首页的视图"""
-    return HttpResponse("hello django")
+    return HttpResponse("Hello Django")
